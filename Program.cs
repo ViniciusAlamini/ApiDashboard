48568b5fcf459a11a0fef1f388ab2e1a54304722
Vinicus Alamini <56409265+Vezaa@users.noreply.github.com>
2024-10-31 05:10:01 -0300

    arquivos movidos

--- Program.cs
+++ Program.cs
@@ -8,15 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DashboardContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Connection")));
 
-// Add services to the container.
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ITicketRepository, TicketRepository>();
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
